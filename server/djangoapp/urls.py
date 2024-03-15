03008cb3cb3c8cb841c601f65dc8f595d0d642e3
sachin singh <sachinsingh2156@gmail.com>
2024-03-15 10:55:12 -0400

    week 3 without sentiment analysis completed

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route="logout", view=views.logout_request, name="logout"),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
     # path for dealer reviews view
 
     # path for add a review view
